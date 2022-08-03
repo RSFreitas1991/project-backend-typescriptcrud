f227b063d63fdcd928566874f9460a17b8cede49
Raphael Freitas <raphael.soarescivil@gmail.com>
2022-08-03 10:16:20 -0300

    Fix: Requisito 3

--- src/models/ordersMode.ts
+++ src/models/ordersMode.ts
@@ -1,16 +1,5 @@
 import db from './connection';
 
-/* const query = `SELECT
-Orders.id id,
-Orders.userId userId,
-GROUP_CONCAT(Products.id) productsIds
-FROM
-Trybesmith.Orders AS Orders
-JOIN Trybesmith.Products AS Products
-ON Orders.id = Products.orderId
-GROUP BY Orders.id
-ORDER BY Orders.id ASC;`; */
-
 const ordersModel = {
   async getAll() {
     const sql = 'SELECT * FROM Trybesmith.Orders;';
--- src/services/usersService.ts
+++ src/services/usersService.ts
@@ -1,5 +1,8 @@
 import jwt from 'jsonwebtoken';
+import * as dotenv from 'dotenv';
 import usersModel from '../models/usersModel';
+
+dotenv.config();
 
 const secret: any = process.env.JWT_SECRET;
 
@@ -9,11 +12,7 @@ const usersService = {
     return token;
   },
   async addUser(user: any) {
-  /*     const token = await this.createToken(user); */
-    const token = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.
-    eyJ1c2VyIjp7InVzZXJuYW1lIjoiY2F0aWF1IiwicGFzc3dvcmQiOiJzZW5oY
-    TEyMzQiLCJsZXZlbCI6MiwiY2xhc3NlIjoiY2xhc3NlIn0sImlhdCI6MTY1OTUzMTM4Nn0.
-    BgypOu2ivAEmEpocJ79P6RwSKMV-O1J8nWJ6SmvwKS8`;
+    const token = await this.createToken(user);
     await usersModel.addUser(user);
     return token;
   },
